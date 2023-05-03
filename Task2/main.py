30c310ddbd508497674877bff63daa36a1849c38
Alterise <alterise.job@gmail.com>
2023-05-03 23:36:47 +0300

    rework task2, add task4

--- Task2/main.py
+++ Task2/main.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-# docker run -d -p 80:80 gunicorn
-
 def app(environ, start_response):
     data = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n\n".encode('utf-8')
     start_response("200 OK", [
