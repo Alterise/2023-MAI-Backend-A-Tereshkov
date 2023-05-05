34ecb78866875629243f97b6114ab89bf2f86936
Alterise <alterise.job@gmail.com>
2023-05-05 20:23:12 +0300

    done

--- Task6/django/app/models.py
+++ Task6/django/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    rarity = models.CharField(max_length=50, verbose_name='Редкость')
+    price = models.IntegerField(verbose_name='Цена')
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Никнейм персонажа')
+    class_name = models.CharField(max_length=10, verbose_name='Класс')
+    lvl = models.IntegerField(verbose_name='Уровень')
+    inventory = models.OneToOneField(Item, on_delete=models.CASCADE, verbose_name='Инвентарь')
+
+
+class Profile(models.Model):
+    username = models.CharField(unique=True, max_length=50, verbose_name='Никнейм')
+    hours_played = models.IntegerField(verbose_name='Количество часов сыграно')
+    characters_count = models.IntegerField(verbose_name='Количество персонажей')
+    characters = models.ManyToManyField(Character, verbose_name='Персонажи')
